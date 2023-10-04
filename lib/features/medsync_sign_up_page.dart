9a71243719220fb2be272b6343ac83e77e5af3f5
Wilmer Suelo <wilmer.suelo@wvsu.edu.ph>
2023-10-05 00:09:01 +0800

    search bar

--- lib/features/medsync_sign_up_page.dart
+++ lib/features/medsync_sign_up_page.dart
@@ -16,26 +16,41 @@ class _MedsyncSignUpPageState extends State<MedsyncSignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.all(45),
         decoration: const BoxDecoration(
           // Add decoration as needed
           color: Color.fromARGB(255, 255, 255, 255),
         ),
         child: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text(
-              'Welcome to Medsync',
+              'Medicine Library',
               style: TextStyle(
                 fontFamily: 'Poppins',
-                fontWeight: FontWeight.w500,
-                fontSize: 35,
+                fontWeight: FontWeight.w800,
+                fontSize: 30,
                 color: Color.fromARGB(255, 25, 23, 149),
               ),
             ),
-            Row(
-              children: [
-                // Add your row content here
-              ],
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SearchBar(
+                     constraints: BoxConstraints(
+                         maxWidth: 250,
+                       ),
+                    leading: Icon(Icons.search),
+                    hintText: 'Search for a medicine',
+                    backgroundColor: MaterialStatePropertyAll(
+                       Color.fromARGB(255, 255, 255, 255)
+                      
+                    ),
+                  )
+                ],
+              ),
             ),
           ],
         ),
